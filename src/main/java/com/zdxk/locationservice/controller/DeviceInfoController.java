4cf517463f15d414359cd199d459f3ac2b75c9b4
P01son6415 <liyudong123@hotmail.com>
2019-02-26 22:00:08 +0800

    feat Swagger

---
+++ src/main/java/com/zdxk/locationservice/controller/DeviceInfoController.java
@@ -0,0 +1,13 @@
+package com.zdxk.locationservice.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api("设备信息")
+@RestController
+@RequestMapping("/device/*")
+public class DeviceInfoController {
+
+
+}
